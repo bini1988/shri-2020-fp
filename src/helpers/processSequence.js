909ffbc5d28fd59c4b1ec80cd73db4a288ae7b17
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-04-11 09:23:45 +0300

    Add processSequence implementation

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,61 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import { __, allPass, assoc, compose, concat, curry, gt, ifElse, length, lt, mathMod, not, otherwise, partial, pipe, prop, startsWith, tap, test, toString } from "ramda";
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+export default ({value, writeLog, handleSuccess, handleError}) => {
+    const then = fn => pr => pr.then(fn);
+    const powTwo = curry(Math.pow)(__, 2);
+    const tapWriteLog = tap(writeLog);
+    const resultProp = prop('result');
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
-
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
-
-    wait(2500).then(() => {
-        writeLog('SecondLog')
-
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
-
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
-
-export default processSequence;
+    pipe(
+        tapWriteLog,
+        ifElse(
+            allPass([
+                compose(gt(10), length),
+                compose(lt(2), length),
+                compose(not, startsWith('-')),
+                test(/^[0-9]+\.?[0-9]+$/i),
+            ]),
+            pipe(
+                pipe(Number, Math.round, tapWriteLog),
+                pipe(
+                    assoc('number', __, { from: 10, to: 2 }),
+                    api.get('https://api.tech/numbers/base'),
+                    then(resultProp),
+                    then(tapWriteLog),
+                    then(length),
+                    then(tapWriteLog),
+                    then(powTwo),
+                    then(tapWriteLog),
+                    then(mathMod(__, 3)),
+                    then(tapWriteLog),
+                    then(toString),
+                    then(concat('https://animals.tech/')),
+                    then(api.get(__, null)),
+                    then(resultProp),
+                    then(handleSuccess),
+                    otherwise(handleError),
+                )
+            ),
+            partial(handleError, ['ValidationError']),
+        )
+    )(value);
+};
